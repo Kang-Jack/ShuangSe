2e1e9fc386a3c1e1c403bfb3d3fba7e68fe9bc34
Kang-Jack <laserk_klei@hotmail.com>
2017-05-18 18:02:10 +0800

    introduce pandas
    introduce pandas

--- python2.7/analyze.py
+++ python2.7/analyze.py
@@ -1,18 +1,25 @@
 import dblottery
+from pandas import DataFrame, Series
+import pandas as pd
+import numpy as np
 debug=1
 db = dblottery.dblottery()
 
 def get_one_year_data_(year):
+    data=[]
     rows =db.query(r"SELECT * FROM lottery.doubleball where doubleball.GENERATE_TIME like '"+year+"%'")
-    #for row in rows:
-    if debug==1: print str(rows[0]["IDENTIFIER"]).strip("\"")
-    if debug==1: print rows[0]["GENERATE_TIME"].strip("\"")
-    if debug==1: print rows[0]["RED1"].strip("\"")
+    for row in rows:
+        data.append(parse_row_data(row))
+        if debug==1: print str(rows[0]["IDENTIFIER"])
+        if debug==1: print rows[0]["GENERATE_TIME"].strip("\"")
+        if debug==1: print rows[0]["RED1"].strip("\"")
+    #if debug==1: print data
+    return data
 
 def get_data_indentifier_range(year_begin,year_end):
     rows =db.query(r"SELECT * FROM lottery.doubleball where doubleball.IDENTIFIER between "+year_begin+" and "+ year_end )
     #for row in rows:
-    if debug==1: print str(rows[0]["IDENTIFIER"]).strip("\"")
+    if debug==1: print str(rows[0]["IDENTIFIER"])
     if debug==1: print rows[0]["GENERATE_TIME"].strip("\"")
     if debug==1: print rows[0]["RED1"].strip("\"")
     if debug==1: print str(rows[len(rows)-1]["IDENTIFIER"]).strip("\"")
@@ -23,13 +30,20 @@ def get_data_indentifier_range(year_begin,year_end):
 def get_all_data():
     rows =db.query(r"SELECT * FROM lottery.doubleball where 1=1 " )
     #for row in rows:
-    if debug==1: print str(rows[0]["IDENTIFIER"]).strip("\"")
+    if debug==1: print str(rows[0]["IDENTIFIER"])
     if debug==1: print rows[0]["GENERATE_TIME"].strip("\"")
     if debug==1: print rows[0]["RED1"].strip("\"")
     if debug==1: print str(rows[len(rows)-1]["IDENTIFIER"]).strip("\"")
     if debug==1: print rows[len(rows)-1]["GENERATE_TIME"].strip("\"")
     if debug==1: print rows[len(rows)-1]["RED1"].strip("\"")
 
+def parse_row_data(row):
+    return [row["IDENTIFIER"],row["GENERATE_TIME"].strip("\""),row["RED1"].strip("\""),\
+    row["RED2"].strip("\""),row["RED3"].strip("\""),row["RED4"].strip("\""),\
+    row["RED5"].strip("\""),row["RED6"].strip("\""),row["BLUE"].strip("\"")]
+
 if __name__ == '__main__':
-    get_one_year_data_('2008')
-    get_data_indentifier_range('2009001','2010160')
+    rs=get_one_year_data_('2008')
+    #get_data_indentifier_range('2009001','2010160')
+    frame =DataFrame(rs)
+    print  frame[0]
