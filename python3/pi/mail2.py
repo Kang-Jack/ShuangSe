a0bb0b26c3e77d4f8d443f24982dae82e773f40d
Kang-Jack <laserk_klei@hotmail.com>
2017-12-08 09:06:00 +0800

    mailing

--- python3/pi/mail2.py
+++ python3/pi/mail2.py
@@ -9,8 +9,14 @@ import os
 
 import mimetypes
 import time
-import email, email.encoders, email.mime.text, email.mime.base
-import email.mime.application
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from email import encoders
 from base64 import encodebytes
 
@@ -28,11 +34,11 @@ class sina_mail(object):
 
     def smtp_sender(self,mail_to):
         # msg = MIMEMultipart('alternative')
-        msg = email.mime.multipart.MIMEMultipart()
+        msg = MIMEMultipart()
         msg['From'] = self.FROM
         msg['To'] = mail_to.encode()
         msg['Subject'] = 'db_'+str(time.time())
-        body = email.mime.text.MIMEText("""Hello, how are you? I am fine.
+        body = MIMEText("""Hello, how are you? I am fine.
               This is a rather nice letter, don't you think?""")
         msg.attach(body)
 
@@ -46,29 +52,63 @@ class sina_mail(object):
 
         # 构造MIMEBase对象做为文件附件内容并附加到根容器
         filename = 'lottery.db'
+        ctype, encoding = mimetypes.guess_type(filename)
+        if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+            ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+        if maintype == 'text':
+            with open(filename) as fp:
+                # Note: we should handle calculating the charset
+                msg = MIMEText(fp.read(), _subtype=subtype)
+        elif maintype == 'image':
+            with open(filename, 'rb') as fp:
+                msg = MIMEImage(fp.read(), _subtype=subtype)
+        elif maintype == 'audio':
+            with open(filename, 'rb') as fp:
+                msg = MIMEAudio(fp.read(), _subtype=subtype)
+        else:
+            with open(filename, 'rb') as fp:
+                msg = MIMEBase(maintype, subtype)
+                msg.set_payload(fp.read())
+            # Encode the payload using Base64
+            encoders.encode_base64(msg)
+        # Set the filename parameter
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        msg.attach(msg)
+
+        # Now send or store the message
+        composed = msg.as_string()
+
+
+
+
+
+
         #ctype, encoding = mimetypes.guess_type(filename)
         #if ctype is None or encoding is not None:
-         #   ctype = 'application/octet-stream'
+        #    ctype = 'application/octet-stream'
         #maintype, subtype = ctype.split('/', 1)
 
         #file_msg = email.mime.base.MIMEBase(maintype, subtype)
 
-        # = open(filename, 'rb')
+        #data = open(filename, 'rb')
         #file_msg.set_payload(data.read())
         #data.close()
         #email.encoders.encode_base64(file_msg)
-        #file_msg = email.MIMEImage.MIMEImage(open(filename, 'rb').read(), subtype)
+
         #print (ctype, encoding)
 
-        fp = open(filename, 'rb')
-        part = email.mime.base.MIMEBase('application', "octet-stream")
-        part.set_payload(encodebytes(fp.read()).decode())
-        fp.close()
-        part.add_header('Content-Transfer-Encoding', 'base64')
-        part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
-        msg.attach(part)  # msg is an instance of MIMEMultipart()
+        #fp = open(filename, 'rb')
+        #part = email.mime.base.MIMEBase('application', "octet-stream")
+        #part.set_payload(encodebytes(fp.read()).decode())
+        #fp.close()
+        #part.add_header('Content-Transfer-Encoding', 'base64')
+        #part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
+        #msg.attach(part)  # msg is an instance of MIMEMultipart()
 
-
+        #msg.attach(file_msg)
 
 
         ## 设置附件头
@@ -86,7 +126,8 @@ class sina_mail(object):
             _sender.ehlo()
             #_sender.starttls()
             _sender.login(self.USER, self.PASS)
-            _sender.sendmail(self.FROM, mail_to, msg.as_string())
+            #_sender.sendmail(self.FROM, mail_to, msg.as_string())
+            _sender.sendmail(self.FROM, mail_to,composed)
             _sender.quit()
 
             print ("Success")
